175a8cfb2ba7fd6bec59f4ed603ef896702fe1b5
Mingxin <mlyu0008@student.monash.edu>
2021-08-26 21:41:30 +1000

    Add statistics animation in home page

---
+++ js/animation.js
@@ -0,0 +1,19 @@
+/*
+	Visual by TEMPLATE STOCK
+	templatestock.co @templatestock
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+(function($) {
+    "use strict";
+    /* -------------------
+    Fun facts counter
+    ---------------------*/
+    $('.counter').counterUp({
+        delay: 8,
+        time: 1400
+    });
+})(jQuery);
+
+
+
