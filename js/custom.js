1051b71f4cb0361fcdd13f157189d8e1d40fabb4
Ce Fang <cfan0025@student.monash.edu>
2021-08-29 00:31:42 +1000

    add dropdown menu for navigation bar. Commented several functions in custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,23 @@
 (function($) {
 
   // Add smooth scrolling to all links in navbar
-  $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
-
-    var hash = this.hash;
-    if (hash) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function() {
-        window.location.hash = hash;
-      });
-    }
-
-  });
-
-  $(".navbar-collapse a").on('click', function() {
-    $(".navbar-collapse.collapse").removeClass('in');
-  });
+  // $(".navbar a,a.btn-appoint, .quick-info li a, .overlay-detail a").on('click', function(event) {
+  //
+  //   var hash = this.hash;
+  //   if (hash) {
+  //     event.preventDefault();
+  //     $('html, body').animate({
+  //       scrollTop: $(hash).offset().top
+  //     }, 900, function() {
+  //       window.location.hash = hash;
+  //     });
+  //   }
+  //
+  // });
+  //
+  // $(".navbar-collapse a").on('click', function() {
+  //   $(".navbar-collapse.collapse").removeClass('in');
+  // });
 
   //jQuery to collapse the navbar on scroll
   $(window).scroll(function() {
