00883e38cdcb60c3590456211dcc2a87d52f3f7f
Ce Fang <cfan0025@student.monash.edu>
2021-09-21 15:17:22 +1000

    change google api key

--- js/interactiveMap.js
+++ js/interactiveMap.js
@@ -4,8 +4,8 @@ function initMap() {
     console.log("callback")
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -37.8136, lng: 144.9631 },
-        zoom: 12,
-        mapTypeControl: false,
-        streetViewControl: false
+        zoom: 12
+        //mapTypeControl: false,
+        //streetViewControl: false
     });
 }
