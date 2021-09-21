94797fdf0f453a01262c8acd8fc38c271fed89b4
Ce Fang <cfan0025@student.monash.edu>
2021-09-21 13:03:04 +1000

    connecting to google map javascript API

---
+++ js/interactiveMap.js
@@ -0,0 +1,11 @@
+let map;
+
+function initMap() {
+    console.log("callback")
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -37.8136, lng: 144.9631 },
+        zoom: 12,
+        mapTypeControl: false,
+        streetViewControl: false
+    });
+}
