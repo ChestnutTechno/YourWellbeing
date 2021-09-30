fb7176e84b370e56628cad08820912f7b28a8c8d
Ce Fang <cfan0025@student.monash.edu>
2021-09-30 20:14:40 +1000

    add recreation sites on the map and related interactions

--- js/interactiveMap.js
+++ js/interactiveMap.js
@@ -1,6 +1,7 @@
 let map;
 let markers = [];
 let tracksLayer;
+let sitesLayer;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: -37.9099, lng: 145.1324 },
@@ -11,13 +12,17 @@ function initMap() {
     // add control divs
     const markersControlDiv = document.createElement("div");
     const tracksControlDiv = document.createElement("div");
+    const sitesControlDiv = document.createElement("div");
     markersControl(markersControlDiv);
     tracksControl(tracksControlDiv);
+    sitesControl(sitesControlDiv);
     map.controls[google.maps.ControlPosition.RIGHT_TOP].push(markersControlDiv);
     map.controls[google.maps.ControlPosition.RIGHT_TOP].push(tracksControlDiv);
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(sitesControlDiv);
 
     addFacilities("data/fac.csv");
     addTracks("data/recweb_track.json");
+    addSites("data/recweb_site.json");
 }
 
 function csvToJSON(string, headers, quoteChar = '"', delimiter = ','){
@@ -40,11 +45,11 @@ This function will add markers and tooltips on the google map that represent rec
 @param: a csv file that stores the geolocation data of recreation facilities
  */
 function addFacilities(file) {
-    var icon = {
+    let icon = {
         url: "img/map_pin_icon.png",
         scaledSize: new google.maps.Size(30, 30),
     }
-    var rawFile = new XMLHttpRequest();
+    let rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, true);
     rawFile.onreadystatechange = function ()
     {
@@ -57,7 +62,7 @@ function addFacilities(file) {
                 for (let i = 0; i < json.length; i++) {
                     const latLng = new google.maps.LatLng(json[i].Latitude, json[i].Longitude);
 
-                    const contentString =
+                    let contentString =
                         '<div>' +
                         '<span style="color: #0f0c1c;font-weight: bold">Name: </span> <span style="color: #0f0c1c">' + json[i]['Facility Name'] + '</span><br><br>' +
                         '<span style="color: #0f0c1c;font-weight: bold">Sports: </span> <span style="color: #0f0c1c">' + json[i]['Sports Played'] + '</span><br><br>' +
@@ -65,11 +70,11 @@ function addFacilities(file) {
                         '<span style="color: #0f0c1c;font-weight: bold">Address: </span> <span style="color: #0f0c1c">' + json[i]['FullAddress'] + '</span><br>' +
                         '</div>'
 
-                    const infowindow = new google.maps.InfoWindow({
+                    let infowindow = new google.maps.InfoWindow({
                         content: contentString,
                     });
 
-                    const marker = new google.maps.Marker({
+                    let marker = new google.maps.Marker({
                         position: latLng,
                         map: map,
                         icon: icon
@@ -98,11 +103,69 @@ function addFacilities(file) {
 function addTracks(file) {
     tracksLayer = new google.maps.Data();
     tracksLayer.loadGeoJson(file);
+
     tracksLayer.setStyle({
        strokeWeight: 5,
-       strokeColor: 'red'
+       strokeColor: "#e08600"
     });
+
+    let tracksInfoWindow = new google.maps.InfoWindow();
+
+    tracksLayer.addListener("mouseover", function(event) {
+        tracksLayer.setStyle({strokeWeight: 10, strokeColor: "#e08600"});
+        let contentString = '<span style="color: #0f0c1c">' + event.feature.getProperty("NAME") + '</span>';
+        tracksInfoWindow.setContent(contentString);
+        tracksInfoWindow.setPosition(event.latLng);
+        tracksInfoWindow.setOptions({pixelOffset: new google.maps.Size(0,-10)});
+        tracksInfoWindow.open(map);
+    });
+
+    tracksLayer.addListener("mousemove", function(event) {
+        tracksInfoWindow.setPosition(event.latLng);
+        tracksInfoWindow.setOptions({pixelOffset: new google.maps.Size(0,-10)});
+    })
+
+    tracksLayer.addListener("mouseout", function() {
+        tracksLayer.setStyle({strokeWeight: 5, strokeColor: "#e08600"});
+        tracksInfoWindow.close();
+    })
+
     tracksLayer.setMap(map);
+
+}
+
+function addSites(file) {
+    let icon = {
+        url: "img/mountain_icon.png",
+        scaledSize: new google.maps.Size(30, 30),
+    }
+    let largerIcon = {
+        url: "img/mountain_icon.png",
+        scaledSize: new google.maps.Size(40, 40),
+    }
+    sitesLayer = new google.maps.Data();
+    sitesLayer.loadGeoJson(file);
+    sitesLayer.setStyle({
+        icon: icon
+    });
+
+    let sitesInfoWindow = new google.maps.InfoWindow();
+
+    sitesLayer.addListener("mouseover", function(event) {
+        let contentString = '<span style="font-weight:bold;color: #0f0c1c">Name: </span><span style="color: #0f0c1c">' + event.feature.getProperty("NAME") + '</span><br><br>' +
+        '<span style="font-weight:bold;color: #0f0c1c">Access Instruction: </span><p style="color: #0f0c1c;text-align: justify;">' + event.feature.getProperty("ACCESS_DSC") + '</p>';
+        sitesInfoWindow.setContent(contentString);
+        sitesInfoWindow.setPosition(event.latLng);
+        sitesInfoWindow.setOptions({
+            pixelOffset: new google.maps.Size(0,-30),
+            maxWidth: 300
+        });
+        sitesInfoWindow.open(map);
+    });
+    sitesLayer.addListener("mouseout", function(event) {
+        sitesInfoWindow.close();
+    });
+    sitesLayer.setMap(map);
 }
 
 function creatControlUI(controlDiv, innerText){
@@ -185,6 +248,24 @@ function tracksControl(controlDiv){
     });
 }
 
+function sitesControl(controlDiv) {
+    const sitesControlUI = creatControlUI(controlDiv, "Hide Sites");
+
+    let show = true;
+    // Setup the click event listeners: simply set the map to Chicago.
+    sitesControlUI.addEventListener("click", () => {
+        let nodes = sitesControlUI.childNodes;
+        if(show){
+            nodes.item(0).innerHTML =  "Show Sites";
+            hideSites();
+            show = false;
+        }else {
+            nodes.item(0).innerHTML =  "Hide Sites";
+            showSites();
+            show = true;
+        }
+    });
+}
 
 function setMapOnAll(map) {
     for (let i = 0; i < markers.length; i++) {
@@ -206,6 +287,14 @@ function hideTracks() {
 
 function showTracks() {
     tracksLayer.setMap(map);
+}
+
+function hideSites() {
+    sitesLayer.setMap(null);
+}
+
+function showSites() {
+    sitesLayer.setMap(map);
 }
 
 
