6f73c99404194cd3b71bced685bf87b241b8b1f4
Ce Fang <cfan0025@student.monash.edu>
2021-10-08 04:18:39 +1100

    optimise hospital map UI and add autocomplete address box

--- js/hospitalMap.js
+++ js/hospitalMap.js
@@ -6,10 +6,15 @@ function initMap() {
     hospitalMap = new google.maps.Map(document.getElementById("hospital_map"), {
         center: {lat: -37.8136, lng: 144.9631},
         zoom: 10,
+        mapId: "28c9117c2aa9ddc6",
         fullscreenControl: false,
         mapTypeControl: false,
         streetViewControl: false
     });
+
+    var inputContainer = document.createElement("div");
+    addAddressInputControl(inputContainer);
+    hospitalMap.controls[google.maps.ControlPosition.TOP_LEFT].push(inputContainer);
 
     icon = {
         url: "img/user_location_icon.png",
@@ -97,7 +102,7 @@ function getRealtimeLocation(){
         userLocation.setPosition(new google.maps.LatLng(lat,lon));
         userLocation.setIcon(icon);
         userLocation.setMap(hospitalMap);
-        hospitalMap.setZoom(13);
+        hospitalMap.setZoom(14);
     });
 }
 
@@ -118,9 +123,98 @@ function getAddress() {
             userLocation.setPosition(new google.maps.LatLng(lat, lon));
             userLocation.setIcon(icon);
             userLocation.setMap(hospitalMap);
-            hospitalMap.setZoom(13);
+            hospitalMap.setZoom(14);
         } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+            if(status == google.maps.GeocoderStatus.ZERO_RESULTS){
+                alert("Could find your address, please try to enter full address e.g. 123 Fake Ave. FakeTown Fake postcode");
+            }
+            if(status == google.maps.GeocoderStatus.ERROR ||
+                status == google.maps.GeocoderStatus.UNKNOWN_ERROR ||
+                status == google.maps.GeocoderStatus.INVALID_REQUEST ||
+                status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT){
+                alert("Oops, some error occurs. Please try again later");
+            }
+            if(status == google.maps.GeocoderStatus.REQUEST_DENIED){
+                alert("Request denied");
+            }
         }
     });
+}
+
+function addAddressInputControl(container) {
+    var addressUI = document.createElement("div");
+    addressUI.setAttribute("id", "add_input_form");
+    addressUI.style.width = "900px";
+    addressUI.style.height = "40px";
+    addressUI.style.marginTop = "-40px";
+    addressUI.style.marginLeft = "10px";
+    container.appendChild(addressUI);
+
+    var inputBox = document.createElement("input");
+    inputBox.setAttribute("class", "card");
+    inputBox.setAttribute("id", "address");
+    inputBox.style.padding = "0 0 0 10px";
+    inputBox.style.marginLeft = "10px";
+    inputBox.style.height = "40px";
+    inputBox.style.width = "80%";
+    inputBox.style.fontSize = "16px";
+    inputBox.style.color = "#0f0c1c";
+    inputBox.style.borderTopRightRadius = "0";
+    inputBox.style.borderBottomRightRadius = "0";
+    inputBox.style.borderColor = "#0f0c1c";
+    inputBox.setAttribute("type", "text");
+    inputBox.setAttribute("placeholder", "Address");
+    container.appendChild(inputBox);
+
+    var center = {lat: -37.8136, lng: 144.9631};
+    const defaultBounds = {
+        north: center.lat + 1,
+        south: center.lat - 1,
+        east: center.lng + 1,
+        west: center.lng - 1,
+    };
+    const options = {
+        bounds: defaultBounds,
+        componentRestrictions: { country: "au" },
+        fields: ["address_components"],
+        strictBounds: false,
+        types: ["address"],
+    };
+    let autoComplete = new google.maps.places.Autocomplete(inputBox, options);
+
+    var iconContainer = document.createElement("span");
+    iconContainer.setAttribute("onclick", "getRealtimeLocation()");
+    iconContainer.style.marginLeft = "-40px";
+    container.appendChild(iconContainer);
+
+    var locationIcon = document.createElement("i");
+    locationIcon.setAttribute("class", "fa fa-location-arrow fa-2x");
+    locationIcon.setAttribute("aria-hidden", "true");
+    iconContainer.appendChild(locationIcon);
+
+    var button = document.createElement("button");
+    button.setAttribute("id", "sub_add");
+    button.setAttribute("class", "nm_btn");
+    button.setAttribute("onclick", "getAddress()");
+    button.textContent = "Search";
+    button.style.borderTopLeftRadius = "0";
+    button.style.borderBottomLeftRadius = "0";
+    button.style.borderTopRightRadius = "10px";
+    button.style.borderBottomRightRadius = "10px";
+    button.style.borderBottomLeftRadius = "0";
+    button.style.width = "15%";
+    button.style.marginLeft = "10px";
+    button.style.fontSize = "16px";
+    container.appendChild(button);
+
+    locationIcon.addEventListener("mouseover", function() {
+        locationIcon.style.color = "#0f0c1c";
+    })
+
+    locationIcon.addEventListener("mouseout", function() {
+        locationIcon.style.color = "";
+    })
+
+
+
 }
